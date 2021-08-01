e1600877772fa45a7c1fad24141adf554cff1e93
Samuel-DeSantis <samuel.i.desantis@gmail.com>
2021-07-31 22:41:13 -0600

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const newForm = document.getElementById('create-task-form');
+  const newDescription = document.getElementById('new-task-description');
+  const newPriority = document.getElementById('new-task-priority');
+  const newUl = document.getElementById('tasks');
+  newForm.addEventListener('submit', createNewTask)
 });
+
+function createNewTask (event) {
+  event.preventDefault();
+  const newDescription = document.getElementById('new-task-description');
+  const newTask = document.createElement('li');
+  newTask.innerText = newDescription.value;
+  appendNewTask(newTask);
+  event.target.reset();
+};
+
+function appendNewTask (task) {
+  document.getElementById('tasks').appendChild(task);
+};
